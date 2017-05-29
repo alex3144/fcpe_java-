f526bc4b618defc947d64531bd776f687a94883f
alex3144 <alexboceno@hotmail.fr>
2017-05-29 13:44:58 +0200

    last commit

--- business/src/main/java/fr/imie/project/emailSAAS/mailjetAPI.java
+++ business/src/main/java/fr/imie/project/emailSAAS/mailjetAPI.java
@@ -1,7 +1,14 @@
 package fr.imie.project.emailSAAS;
 
+import com.mailjet.client.MailjetClient;
+
 /**
  * Created by max on 29/05/2017.
  */
+
+
 public class mailjetAPI {
+
+    MailjetClient client = new MailjetClient(System.getenv("c8dac4f06293b116e44b43653ef8f823"), System.getenv("c6873cb7acade767778ea21cb5c4d91f"));
+
 }
